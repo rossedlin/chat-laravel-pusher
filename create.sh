2608636ed14afab828c4740ed34d7901bda504f7
Ross Edlin <r.edlin@ucl.ac.uk>
2022-10-24 11:26:37 +0100

    Add up/down.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#docker compose run web bash
 docker compose run web composer create-project --prefer-dist laravel/laravel chat-laravel-pusher
---
+++ up.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+docker-compose down
+docker container prune -f
+docker-compose up
